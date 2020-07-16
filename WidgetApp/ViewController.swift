a582fd6146fc093df1092a9c51c63edfbdcab625
mahmoud gaber <mahmoudbasuni90@gmail.com>
2020-07-16 18:06:00 +0200

    Initial Commit

---
+++ WidgetApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WidgetApp
+//
+//  Created by Bassuni on 7/16/20.
+//  Copyright © 2020 futureface. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
