1440a175bfe4ffa07df97bc3179ebaa98d320e8a
mahmoud gaber <mahmoudbasuni90@gmail.com>
2020-07-21 04:02:21 +0200

    .

---
+++ WidgetApp/Helpers/UITableViewCellExt.swift
@@ -0,0 +1,29 @@
+//
+//  UITableViewCellExt.swift
+//  WidgetApp
+//
+//  Created by Bassuni on 7/19/20.
+//  Copyright © 2020 futureface. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UITableView{
+
+    func getCell(identifier: String, for indexPath: IndexPath) -> UITableViewCell {
+
+        let cell = self.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
+
+        return cell
+    }
+
+    func getCell(identifier: String) -> UITableViewCell {
+
+        guard let cell = self.dequeueReusableCell(withIdentifier: identifier) else {
+                   fatalError("Cell not exists in storyboard")}
+
+        return cell
+    }
+
+}
